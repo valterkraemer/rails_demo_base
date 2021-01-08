e929a9f264e3607b52a30b0ac0e1204bb3dc46b7
prafullpurwar <prafullpurwar@gmail.com>
2021-01-08 16:50:01 +0530

    Adding reusable componets for table and header

---
+++ app/javascript/components/useTable.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { Table, TableHead, TableRow, TableCell, makeStyles, TablePagination, TableSortLabel } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+    table: {
+        marginTop: theme.spacing(3),
+        '& thead th': {
+            fontWeight: '600',
+            color: theme.palette.primary.main,
+            backgroundColor: theme.palette.primary.light,
+        },
+        '& tbody td': {
+            fontWeight: '300',
+        },
+        '& tbody tr:hover': {
+            backgroundColor: '#fffbf2',
+            cursor: 'pointer',
+        },
+    },
+}))
+
+export default function useTable(records, headCells,filterFn, setSortingOrder) {
+
+    const classes = useStyles();
+
+    
+    const [order, setOrder] = useState()
+    const [orderBy, setOrderBy] = useState()
+
+    const TblContainer = props => (
+        <Table className={classes.table}>
+            {props.children}
+        </Table>
+    )
+
+    const TblHead = props => {
+
+        const handleSortRequest = cellId => {
+            const isAsc = orderBy === cellId && order === "asc";
+            setOrder(isAsc ? 'desc' : 'asc');
+            setOrderBy(cellId)
+            cellId === 'users_count' && setSortingOrder(isAsc ? 'desc' : 'asc')
+
+        }
+
+        return (<TableHead>
+            <TableRow>
+                {
+                    headCells.map(headCell => (
+                        <TableCell key={headCell.id}
+                            sortDirection={orderBy === headCell.id ? order : false}>
+                            {headCell.disableSorting ? headCell.label :
+                                <TableSortLabel
+                                    active={orderBy === headCell.id}
+                                    direction={orderBy === headCell.id ? order : 'asc'}
+                                    onClick={() => { handleSortRequest(headCell.id) }}>
+                                    {headCell.label}
+                                </TableSortLabel>
+                            }
+                        </TableCell>))
+                }
+            </TableRow>
+        </TableHead>)
+    }
+
+
+    function stableSort(array, comparator) {
+        const stabilizedThis = array.map((el, index) => [el, index]);
+        stabilizedThis.sort((a, b) => {
+            const order = comparator(a[0], b[0]);
+            if (order !== 0) return order;
+            return a[1] - b[1];
+        });
+        return stabilizedThis.map((el) => el[0]);
+    }
+
+    return {
+        TblContainer,
+        TblHead
+    }
+}
